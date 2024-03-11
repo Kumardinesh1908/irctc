30764686f4a24a6e590e5fa0e68a0c0ff36d8ed9
Dinesh Kumar <Kumardinesh1908@gmail.com>
2024-03-11 16:27:57 +0530

    Creating header for Homepage

--- src/Compnents/Homepage.js
+++ src/Compnents/Homepage.js
@@ -9,13 +9,13 @@ const Homepage = () => {
                 <div className=''>
                     <img src={indianrailway} alt='' className='w-20' />
                 </div>
-                <div className='flex flex-row gap-5'>
-                    <div className='border w-16 h-8'>
-                        <p>Login</p>
+                <div className='w-[30rem] justify-between items-center flex flex-row'>
+                    <div className='border w-16 h-9  bg-blue-800 '>
+                        <p className='text-white pt-1'>LOGIN</p>
                     </div>
-                    <div>Register</div>
-                    <div>Contact Us</div>
-                    <div>Date</div>
+                    <div className='text-lg uppercase'>Register</div>
+                    <div className='text-lg uppercase'>Contact Us</div>
+                    <div className='text-lg uppercase'>Date</div>
                 </div>
                 <div className=''>
                     <img src={irctc} alt='' className='w-36' />
